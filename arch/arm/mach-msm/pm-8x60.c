1e26b125f84ca5771d7525dabe6e9c3bf089cd9e
dhacker29 <davidhackerdvm@gmail.com>
2013-08-22 23:29:12 -0500

    Revert "msm: pm-8x60: Save and restore the GIC_CPU_CTRL during power collapse"
    This reverts commit e78dfb677f8032cf6a8112e0459497166aed073b.

--- arch/arm/mach-msm/pm-8x60.c
+++ arch/arm/mach-msm/pm-8x60.c
@@ -524,10 +524,6 @@ static bool __ref msm_pm_spm_power_collapse(
 	void *entry;
 	bool collapsed = 0;
 	int ret;
-	unsigned int saved_gic_cpu_ctrl;
-
-	saved_gic_cpu_ctrl = readl_relaxed(MSM_QGIC_CPU_BASE + GIC_CPU_CTRL);
-	mb();
 
 	if (MSM_PM_DEBUG_POWER_COLLAPSE & msm_pm_debug_mask)
 		pr_info("CPU%u: %s: notify_rpm %d\n",
@@ -558,9 +554,7 @@ static bool __ref msm_pm_spm_power_collapse(
 #endif
 		cpu_init();
 		writel(0xF0, MSM_QGIC_CPU_BASE + GIC_CPU_PRIMASK);
-		writel_relaxed(saved_gic_cpu_ctrl,
-				MSM_QGIC_CPU_BASE + GIC_CPU_CTRL);
-		mb();
+		writel(1, MSM_QGIC_CPU_BASE + GIC_CPU_CTRL);
 		local_fiq_enable();
 	}
 
