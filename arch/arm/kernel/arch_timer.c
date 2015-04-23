c829e75199943e56f88b2b4b63bf298aad81135c
CWGSM3VO <cwasser@telus.net>
2015-04-23 15:00:49 -0600

    drivers: clocksource: add CPU PM notifier for ARM architected timer
    Few control settings done in architected timer as part of initialisation
    can be lost when CPU enters deeper power states. They need to be
    restored when the CPU is (warm)reset again.
    
    This patch adds CPU PM notifiers to save the counter control register
    when entering low power modes and restore it when CPU exits low power.
    
    Change-Id: Ic617dc1a8efd0c5df7a31fb9990b6ead51292df6
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Reviewed-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    Reviewed-by: Will Deacon <will.deacon@arm.com>
    Acked-by: Olof Johansson <olof@lixom.net>
    Signed-off-by: Sudeep KarkadaNagesha <sudeep.karkadanagesha@arm.com>
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    Git-commit 346e7480f1d4740b3d798da60f83f087ea6488b4
    Signed-off-by: Trilok Soni <tsoni@codeaurora.org>
    [schikk@codeaurora.org: 3.4 kernel doesn't have
    driver/clocksource/arm_arch_timer.c changes has been done in
    arch/arm/kernel/arch_timer.c ]
    Signed-off-by: Swetha Chikkaboraiah <schikk@codeaurora.org>

--- arch/arm/kernel/arch_timer.c
+++ arch/arm/kernel/arch_timer.c
@@ -15,6 +15,7 @@
 #include <linux/device.h>
 #include <linux/smp.h>
 #include <linux/cpu.h>
+#include <linux/cpu_pm.h>
 #include <linux/jiffies.h>
 #include <linux/clockchips.h>
 #include <linux/interrupt.h>
@@ -381,6 +382,33 @@ static struct local_timer_ops arch_timer_ops __cpuinitdata = {
 	.stop	= arch_timer_stop,
 };
 
+#ifdef CONFIG_CPU_PM
+static unsigned int saved_cntkctl;
+static int arch_timer_cpu_pm_notify(struct notifier_block *self,
+				unsigned long action, void *hcpu)
+{
+	if (action == CPU_PM_ENTER)
+		saved_cntkctl = arch_timer_get_cntkctl();
+	else if (action == CPU_PM_ENTER_FAILED || action == CPU_PM_EXIT)
+		arch_timer_set_cntkctl(saved_cntkctl);
+	return NOTIFY_OK;
+}
+
+static struct notifier_block arch_timer_cpu_pm_notifier = {
+	.notifier_call = arch_timer_cpu_pm_notify,
+};
+
+static int __init arch_timer_cpu_pm_init(void)
+{
+	return cpu_pm_register_notifier(&arch_timer_cpu_pm_notifier);
+}
+#else
+static int __init arch_timer_cpu_pm_init(void)
+{
+	return 0;
+}
+#endif
+
 static int __init arch_timer_common_register(void)
 {
 	int err;
@@ -434,13 +462,20 @@ static int __init arch_timer_common_register(void)
 		}
 	}
 
-	err = local_timer_register(&arch_timer_ops);
+	err = arch_timer_cpu_pm_init();
 	if (err)
 		goto out_free_irq;
+
+	err = local_timer_register(&arch_timer_ops);
+	if (err)
+		goto out_unreg_notify;
+
 	percpu_timer_setup();
 
 	return 0;
 
+out_unreg_notify:
+	cpu_pm_unregister_notifier(&arch_timer_cpu_pm_notifier);
 out_free_irq:
 	free_percpu_irq(arch_timer_ppi, arch_timer_evt);
 	if (arch_timer_ppi2)
